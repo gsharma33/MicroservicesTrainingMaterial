e75f8a8d204ec4fcca1ed257cc36acb1d1ca71c9
gsharma33 <83568381+gsharma33@users.noreply.github.com>
2021-05-06 12:17:29 +0100

    Delete CustomerRepository.java

--- CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.boa.upload.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.boa.upload.models.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer,Long>{
-
-}
